c445264b216d339a73aac4d7a8d1648a2b42ec2e
Simon Masooglu <masooglu.simon@gmail.com>
2024-04-09 22:06:04 +0200

    Removed await

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-await mongoose.connect(process.env.MONGODB_URI).then(() => {
+mongoose.connect(process.env.MONGODB_URI).then(() => {
     console.log("Connection to MongoDB was successful.")
 }).catch((err) => console.error(err))
