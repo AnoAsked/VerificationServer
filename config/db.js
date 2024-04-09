2f09704745046f4c230b6c7e1add9422249c9723
Simon Masooglu <masooglu.simon@gmail.com>
2024-04-09 21:42:07 +0200

    added await to mongoose init

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI).then(() => {
+await mongoose.connect(process.env.MONGODB_URI).then(() => {
     console.log("Connection to MongoDB was successful.")
 }).catch((err) => console.error(err))
