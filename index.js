e2b6f40efb145b5d96a8dfcfb533aae70a672cb0
Simon Masooglu <masooglu.simon@gmail.com>
2024-04-10 16:05:47 +0200

    Added Access-Control-Allow-Origin

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ const bodyParser = require('express').json;
 const cors = require('cors')
 app.use(bodyParser());
 app.use('/verification', verificationRouter);
-app.use(cors())
+app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.listen(port, () => console.log(`AnoAsked verification server running on port: ${port}`))
