126ff9f279144b1a49518c3dc29327aeb2d4f898
Simon Masooglu <masooglu.simon@gmail.com>
2024-04-10 15:56:23 +0200

    Added cors

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const port = 3000;
 const verificationRouter = require('./api/verification')
 
 const bodyParser = require('express').json;
+const cors = require('cors')
 app.use(bodyParser());
 app.use('/verification', verificationRouter);
+app.use(cors())
 
 app.listen(port, () => console.log(`AnoAsked verification server running on port: ${port}`))
