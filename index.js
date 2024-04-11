1e7f717c41d1303732b8620468337eb63d7b6666
Simon Masooglu <masooglu.simon@gmail.com>
2024-04-11 03:17:54 +0200

    removed /auth dub from cors origins list

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const verificationRouter = require('./api/verification')
 const bodyParser = require('express').json;
 const cors = require('cors')
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://anoasked-git-dev-simon-masooglus-projects.vercel.app/auth', 'https://anoasked.vercel.app']
+    origin: ['http://localhost:5173', 'https://anoasked-git-dev-simon-masooglus-projects.vercel.app', 'https://anoasked.vercel.app']
 }));
 app.use(bodyParser());
 app.use('/verification', verificationRouter);
